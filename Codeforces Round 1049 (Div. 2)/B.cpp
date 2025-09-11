2c15fa6a3dbb0ffbaea258f02f788e94917b89e1
Youngha Park (박영하) <geoffrey.youngha.park@gmail.com>
2025-09-11 13:07:11 +0900

    Create B.cpp

---
+++ Codeforces Round 1049 (Div. 2)/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int x;
+    cin >> x;
+    cout << 2 * x << '\n';
+  }
+  return 0;
+}
